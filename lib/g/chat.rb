9426e16ef22acd330d489b449800a995e8b316e0
Gunjan Solanki <solanki.gunjan007@gmail.com>
2021-04-17 21:40:22 +0530

    Initial commit

---
+++ lib/g/chat.rb
@@ -0,0 +1,8 @@
+require "g/chat/version"
+
+module G
+  module Chat
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/g/chat/version.rb
@@ -0,0 +1,5 @@
+module G
+  module Chat
+    VERSION = "0.1.0"
+  end
+end
